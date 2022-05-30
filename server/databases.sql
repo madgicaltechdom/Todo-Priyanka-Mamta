ee2865b315ef38fefa7163e3bdebc9a5f37e4c26
rajputmamta <102136655+rajputmamta@users.noreply.github.com>
2022-05-30 12:06:31 +0530

    Create databases.sql

---
+++ server/databases.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE users (
+--   ID SERIAL PRIMARY KEY,
+--   name VARCHAR(30),
+--   email VARCHAR(30)
+-- );
+
+
+
+CREATE TABLE loginuser (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  url VARCHAR(30),
+  email VARCHAR(30)
+);
